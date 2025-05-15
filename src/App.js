07d5f478bad39fcfc7be492cd65febeaef952bb4
YimYoungheun <dudgms0318@naver.com>
2025-05-16 00:12:58 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -383,29 +383,28 @@ function App() {
                                   style={{ border: '1px solid #ccc', borderRadius: '12px', marginBottom: '1rem' }}
                                   title="파일 업로드"
                                 />
-    
+                                
                                 <button
                                   style={{ background: 'black', color: 'white', padding: '0.5rem 1rem', borderRadius: '6px', border: 'none', cursor: 'pointer' }}
                                   onClick={() => alert('기재해주신 연락처로 담당자가 연락할 수 있습니다.')}
                                 >
                                   확인
                                 </button>
-                              </div>
-                              </>
-                            )}
-                          </>
-                        )}
-                      </>
-                    )}
-                  </>
-                )}
-              </>
-            )}
+                              </div> {/* 이 div는 위에서 연 <div>를 닫아주는 것 - 반드시 있어야 함 */}
+                            </>
+                          )}
+                        </>
+                      )}
+                    </>
+                  )}
+                </>
+              )}
           </div>
         </div>
       );
-    }
-    
-    export default App;
+      }
+
+export default App;
+
     
   
