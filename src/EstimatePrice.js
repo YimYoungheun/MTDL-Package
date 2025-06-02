ec58b195a7f9a26d651b2b2fb7be8c4115e2b159
YimYoungheun <dudgms0318@naver.com>
2025-06-02 18:01:54 +0900

    Update EstimatePrice.js

--- src/EstimatePrice.js
+++ src/EstimatePrice.js
@@ -216,11 +216,18 @@ const EstimatePrice = ({
   const unitPriceWithMargin = Math.ceil(estimateExceptDiecutWithMargin / totalQuantity);
 
   return (
-    <div style={{ margin: '1rem 0', color: 'crimson', fontWeight: 'bold', fontSize: '1.3rem' }}>
-      예상 견적: {estimateWithMargin.toLocaleString()}원<br />
-      개당 금액(목형칼비 제외): {unitPriceWithMargin.toLocaleString()}원
-    </div>
-  );
-};
-
+  <div style={{ margin: '1rem 0', color: 'crimson', fontWeight: 'bold', fontSize: '1.3rem' }}>
+    예상 견적: {estimateWithMargin.toLocaleString()}원
+    <br />
+    <span style={{ 
+      color: 'black', 
+      fontWeight: 'normal', 
+      fontSize: '1rem', 
+      display: 'block', 
+      marginTop: '0.5rem'
+    }}>
+      개당 금액: {unitPriceWithMargin.toLocaleString()}원
+    </span>
+  </div>
+);
 export default EstimatePrice;
