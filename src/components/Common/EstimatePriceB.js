55f76435215b7ca5b4735a720f3c433b912221a3
YimYoungheun <dudgms0318@naver.com>
2025-06-08 18:34:51 +0900

    Update EstimatePriceB.js

--- src/components/Common/EstimatePriceB.js
+++ src/components/Common/EstimatePriceB.js
@@ -61,11 +61,11 @@ function getCoatingFee(coatingType, totalQty, perSheetCount) {
   if (!coatingType || coatingType === '없음') return 0;
   const sheetCount = Math.ceil(totalQty / perSheetCount);
   if (coatingType === '벨벳') {
-    if (sheetCount <= 500) return 200000;
-    return sheetCount * 800;
+    if (sheetCount <= 250) return 200000;
+    return sheetCount * 280;
   }
-  if (sheetCount <= 200) return 70000;
-  return sheetCount * 400;
+  if (sheetCount <= 250) return 70000;
+  return sheetCount * 140;
 }
 
 function getThomsonFee(totalQty, perSheetCount) {
